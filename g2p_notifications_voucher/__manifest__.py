61e6f993723fa2c831595dfed4f6cbdc3d3914eb
Lalith Kota <kotalalith@gmail.com>
2023-05-30 03:00:33 +0530

    Added Notification Voucher module.

--- g2p_notifications_voucher/__manifest__.py
+++ g2p_notifications_voucher/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "G2P Notifications: Fast2SMS Service Provider",
+    "name": "G2P Notifications: Voucher",
     "category": "G2P",
     "version": "15.0.1.1.0",
     "sequence": 1,
@@ -8,11 +8,11 @@
     "license": "Other OSI approved licence",
     "development_status": "Alpha",
     "depends": [
+        "g2p_entitlement_voucher",
         "g2p_notifications_base",
     ],
     "data": [
-        "views/fast2sms_notification_manager.xml",
-        "security/ir.model.access.csv",
+        "views/email_notification_manager.xml",
     ],
     "assets": {
         "web.assets_backend": [],
