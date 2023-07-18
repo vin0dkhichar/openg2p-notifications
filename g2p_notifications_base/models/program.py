6e2bc2d9b89f16881db3f5ae454f5e7806f18708
manoj-02 <mkumar6@ch.iitr.ac.in>
2023-07-18 10:23:04 +0530

    G2P-973 Displaying error message if Nofitication manager is not configured

--- g2p_notifications_base/models/program.py
+++ g2p_notifications_base/models/program.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import models
+from odoo import _, models
 
 _logger = logging.getLogger(__name__)
 
@@ -15,8 +15,23 @@ class G2PProgram(models.Model):
             for mem in self.program_membership_ids
             if mem.state in ("enrolled",) and not mem.is_enrolled_notification_sent
         ]
-        for manager in self.notification_managers:
-            if manager.manager_ref_id:
-                manager.manager_ref_id.on_enrolled_in_program(partners_to_notify)
-        for mem in partners_to_notify:
-            mem.is_enrolled_notification_sent = True
+        if self.notification_managers:
+            for manager in self.notification_managers:
+                if manager.manager_ref_id:
+                    manager.manager_ref_id.on_enrolled_in_program(partners_to_notify)
+            for mem in partners_to_notify:
+                mem.is_enrolled_notification_sent = True
+        else:
+            return {
+                "type": "ir.actions.client",
+                "tag": "display_notification",
+                "params": {
+                    "title": _("Notification"),
+                    "message": _("No Notification Manager defined."),
+                    "sticky": False,
+                    "type": "danger",
+                    "next": {
+                        "type": "ir.actions.act_window_close",
+                    },
+                },
+            }
