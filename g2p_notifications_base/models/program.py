825179e354f550e5615222b25e7751bb6ca8fc38
Lalith Kota <kotalalith@gmail.com>
2023-07-28 20:59:32 +0530

    Modified managers get using get_managers api call

--- g2p_notifications_base/models/program.py
+++ g2p_notifications_base/models/program.py
@@ -3,6 +3,8 @@ import logging
 from odoo import _, models
 from odoo.exceptions import UserError
 
+from odoo.addons.g2p_programs.models import constants
+
 _logger = logging.getLogger(__name__)
 
 
@@ -16,9 +18,10 @@ class G2PProgram(models.Model):
             for mem in self.program_membership_ids
             if mem.state in ("enrolled",) and not mem.is_enrolled_notification_sent
         ]
-        if self.notification_managers:
-            for manager in self.notification_managers:
-                if manager.manager_ref_id:
+        notification_managers = self.get_managers(constants.MANAGER_NOTIFICATION)
+        if notification_managers:
+            for manager in notification_managers:
+                if manager:
                     manager.manager_ref_id.on_enrolled_in_program(partners_to_notify)
             for mem in partners_to_notify:
                 mem.is_enrolled_notification_sent = True
